d5f50bb83f168cc3d8f0f5af1f09dac482e3c265
Dhaval Purohit <dhaval.purohit08@gmail.com>
2016-08-02 00:49:01 +0530

    adds get request for authentication

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,22 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-	actions: {
-		beginTest() {
-			$.ajax({
-				async: true,
-				crossDomain: true,
-				url: "http://localhost:8000/authenticate",
-				method: "GET",
-				headers: {
-				    Authorization: "test-2345",
-				},
-				success: function(response){
-					console.log(response);
-				}
-			});
-			
-		}
-	}
-
 });
